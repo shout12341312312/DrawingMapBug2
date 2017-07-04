5725147985482ef9e28791b9806d57cbac8e9afb
shout12341312312 <sc5shout@gmail.com>
2017-07-04 23:50:58 +0200

    Add files via upload

---
+++ Lets Make a Platformer in SDL2 old/Lets Make a Platformer in SDL2/Layer.h
@@ -0,0 +1,38 @@
+﻿/*Written 2017 by Michał Przekota.
+To the extent possible under law, the author(s) have dedicated all copyright to men who want to help with the problem.
+*/
+#pragma once
+#include "Tile.h"
+#include "FileManager.h"
+#include <string>
+#include <algorithm>
+
+class Layer
+{
+public:
+	Layer();
+	~Layer();
+
+	void LoadContent(std::string layerID, std::string mapID, SDL_Renderer *renderer);
+	void UnloadContent();
+	void Update(Player &player);
+	void Draw(SDL_Renderer *renderer);
+private:
+
+	FileManager file;
+
+	Tile tile;
+
+	std::vector<std::vector<std::string>> attributes;
+	std::vector<std::vector<std::string>> contents;
+
+	std::vector<std::pair<int, int>> solidTiles;
+	std::vector<Tile> tiles;
+
+	std::pair<int, int> SetTiles(std::string tileString);
+
+	std::string tileSheet;
+
+	float posX;
+	float posY;
+};
